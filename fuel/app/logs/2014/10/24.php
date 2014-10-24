687be47b241bbf9092be73004e455a4d47751330
boniface pereira <bonifacepereira@gmail.com>
2014-10-24 23:31:09 +0530

    add dummy navigation links

---
+++ fuel/app/logs/2014/10/24.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-10-24 12:54:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-24 12:54:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-24 12:55:05 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-24 12:55:07 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-10-24 13:00:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
