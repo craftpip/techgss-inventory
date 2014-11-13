285ef4d86d058444f7208f876f895263e91b3c65
Gaurish Rane <megaurishrane@gmail.com>
2014-11-13 11:45:26 +0530

    problem to access the get function in side variable

--- public/assets/js/views/item.js
+++ public/assets/js/views/item.js
@@ -1,55 +1,62 @@
 //Filename: boilerplate.js
 
 define([
-  'text!template/item/main.html',
-  'text!template/item/addedit.html'
-], function(main,itemadd){
-  var view = Backbone.View.extend({
-    el:'#page-wrapper',
-    events: {
-      'click .vehical-btn-delete': 'delete',
-      'submit #vehical-create-new-vehical': 'submit'
-    },
-  	render: function(edit){
-  		this.$el.html(main);
-  		// this.rendervehicallist();
-  		 this.renderitemAddEdit(edit);
-  	},
-   renderitemAddEdit: function(id){
-        if(id){
+    'text!template/item/main.html',
+    'text!template/item/addedit.html'
+], function(main, itemadd) {
+    var view = Backbone.View.extend({
+        el: '#page-wrapper',
+        events: {
+            'click .vehical-btn-delete': 'delete',
+            'submit #vehical-create-new-vehical': 'submit'
+        },
+        render: function(edit) {
+            this.$el.html(main);
+            // this.rendervehicallist();
+            this.renderitemAddEdit(edit);
+        },
+        renderitemAddEdit: function(id) {
+            if (id) {
 
-        }else{
-            var category,vehical;
-             func.getData({
-                url: 'api/category/i',
-                success: function (data) {
-                    category = data;
-                    console.log(data);
-                }
-            });
-            func.getData({
-                url: 'api/vehical/i',
-                success: function (data) {
-                    vehical = data;
-                    console.log(data);
-                }
-            });
-            func.getData({
-                url: 'api/dom/i',
-                success: function (data) {
-                    vehical = data;
-                    console.log(data);
-                }
-            });
-            
-            var datavar = _.template(itemadd);
-            
-            var cmdata =  datavar({data: {},
-                                   category:  category});
-            $('#vehical-addedit').html(cmdata);
+            } else {
+                var category;
+                
+                $.get("api/category/i", function(data) {
+                    data = $.parseJSON(data);
+                    $.each(data, function(index, value) {
+                         console.log(value.category_id);
+                    });
+                });
+                
+                 $.get("api/vehical/i", function(data) {
+                    data = $.parseJSON(data);
+                    $.each(data, function(index, value) {
+                         console.log(value.vehical_id);
+                    });
+                });
+                
+//                func.getData({
+//                    url: 'api/category/i',
+//                    success: function(data) {
+//                        console.log(data);
+//                        console.log(that.category);
+//                        ///category = data;
+//                        //return data;
+//                    }
+//                });
+
+                var datavar = _.template(itemadd);
+
+                console.log(category);
+//                $.each(category, function(index, value) {
+//                    // console.log(value);
+//                });
+                var cmdata = datavar({data: {},
+                    category: category});
+                $('#vehical-addedit').html(cmdata);
+            }
         }
-   }
-  });
-  window.item = window.item || new view();
-  return window.item;
+    });
+    window.item = window.item || new view();
+    return window.item;
 });
